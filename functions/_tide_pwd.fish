34e5ab1c770d51cb2814f948955f70bdae45f3db
Sébastien David (seb) <50570955+alk-sdavid@users.noreply.github.com>
2023-09-14 10:12:00 +0200

    feat: customize pwd function for sources directory

--- functions/_tide_pwd.fish
+++ functions/_tide_pwd.fish
@@ -7,7 +7,7 @@ set -l home_icon $tide_pwd_icon_home' '
 set -l pwd_icon $tide_pwd_icon' '
 
 eval "function _tide_pwd
-    if set -l split_pwd (string replace -r '^$HOME' '~' -- \$PWD | string split /)
+    if set -l split_pwd (string replace -r '^$HOME' '~' -- \$PWD | string replace -r '^~/sources/' '/' | string split /)
         test -w . && set -f split_output \"$pwd_icon\$split_pwd[1]\" \$split_pwd[2..] ||
             set -f split_output \"$unwritable_icon\$split_pwd[1]\" \$split_pwd[2..]
         set split_output[-1] \"$color_anchors\$split_output[-1]$reset_to_color_dirs\"
@@ -18,7 +18,7 @@ eval "function _tide_pwd
     string join / -- \$split_output | string length -V | read -g _tide_pwd_len
 
     i=1 for dir_section in \$split_pwd[2..-2]
-        string join -- / \$split_pwd[..\$i] | string replace '~' $HOME | read -l parent_dir # Uses i before increment
+        string join -- / \$split_pwd[..\$i] | string replace '/' '~/sources/' | string replace '~' $HOME | read -l parent_dir # Uses i before increment
 
         math \$i+1 | read i
 
